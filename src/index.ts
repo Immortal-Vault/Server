68db33de2e3544864d521466ca77914795d91d96
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-06-22 15:39:36 +0300

    feat: prisma & express init

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+﻿import { PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+app.post('/init', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            name: 'admin',
+            email: 'admin@gmail.com',
+            password: 'test'
+        },
+    })
+
+    const allUsers = await prisma.user.findMany({});
+    res.status(200).json(allUsers);
+})
+
+app.get('/ping', async (req, res) => {
+    res.status(200).send('Pong!');
+})
+
+app.listen(3000, () => console.log(`🚀 Server ready at: http://localhost:3000`))
