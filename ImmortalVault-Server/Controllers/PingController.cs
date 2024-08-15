15469febafd53c7c829d0a896744b4101b92ad56
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-08-16 00:04:35 +0300

    feat: asp project init

---
+++ ImmortalVault-Server/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImmortalVault_Server.Controllers;
+
+[ApiController]
+[Route("api/ping")]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Pong";
+    }
+}
