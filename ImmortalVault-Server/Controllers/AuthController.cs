4f5af30288cbfdd17613931f4bdddcaf7e782532
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-08-16 11:52:06 +0300

    feat: jwt auth

---
+++ ImmortalVault-Server/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using ImmortalVault_Server.Models;
+using ImmortalVault_Server.Services.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImmortalVault_Server.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        this._authService = authService;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginModel model)
+    {
+        if (model is { Username: "demo", Password: "password" })
+        {
+            var token = this._authService.GenerateAccessToken(model.Username, "IMMORTAL_VAULT_CLIENT");
+            return Ok(new { AccessToken = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
+        return Unauthorized("Invalid credentials");
+    }
+}
--- ImmortalVault-Server/Program.cs
+++ ImmortalVault-Server/Program.cs
@@ -1,4 +1,8 @@
+using System.Text;
+using ImmortalVault_Server.Services.Auth;
 using ImmortalVault_Server.Services.Client;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +13,30 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = configuration["JWT:ISSUER"],
+            ValidAudience = configuration["JWT:IMMORTAL_VAULT_CLIENT:AUDIENCE"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                configuration["JWT:IMMORTAL_VAULT_CLIENT:SECRET_KEY"] ??
+                throw new Exception("Secret key not configured")))
+        };
+    });
+
 builder.Services.AddSingleton<IClientService, ClientService>();
+builder.Services.AddSingleton<IAuthService, AuthService>();
 
 var app = builder.Build();
 
