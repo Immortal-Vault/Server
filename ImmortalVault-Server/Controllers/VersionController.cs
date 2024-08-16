e5c5ac1354de39700db93b9396662846ea0ade99
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-08-16 10:28:55 +0300

    feat: clientService, versionController

---
+++ ImmortalVault-Server/Controllers/VersionController.cs
@@ -0,0 +1,41 @@
+﻿using ImmortalVault_Server.Services.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImmortalVault_Server.Controllers;
+
+[ApiController]
+[Route("api/version")]
+public class VersionController
+{
+    private readonly IClientService _clientService;
+    private readonly IConfiguration _configuration;
+
+    public VersionController(IClientService clientService, IConfiguration configuration)
+    {
+        this._clientService = clientService;
+        this._configuration = configuration;
+    }
+
+    [HttpGet("downloadUrl")]
+    public async Task<IActionResult> GetDownloadUrl()
+    {
+        var repositoryOwner = this._configuration["REPOSITORY_OWNER"];
+        var repositoryName = this._configuration["REPOSITORY_NAME"];
+
+        if (repositoryOwner is null || repositoryName is null)
+        {
+            return new StatusCodeResult(500);
+        }
+
+        var version = await this._clientService.GetLatestVersion(repositoryOwner, repositoryName);
+
+        if (version is null)
+        {
+            return new StatusCodeResult(500);
+        }
+
+        var downloadUrl = this._clientService.BuildDownloadUrl(version, repositoryOwner, repositoryName);
+
+        return new JsonResult(new { downloadUrl, version = version.Replace("v", "") });
+    }
+}
---
+++ ImmortalVault-Server/Services/Client/ClientService.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace ImmortalVault_Server.Services.Client;
+
+public interface IClientService
+{
+    Task<string?> GetLatestVersion(string repositoryOwner, string repositoryName);
+    string BuildDownloadUrl(string version, string repositoryOwner, string repositoryName);
+}
+
+public class ClientService : IClientService
+{
+    private readonly IConfiguration _configuration;
+    private readonly HttpClient _httpClient = new();
+
+    public ClientService(IConfiguration configuration)
+    {
+        this._configuration = configuration;
+    }
+
+    public string BuildDownloadUrl(string version, string repositoryOwner, string repositoryName)
+    {
+        return $"https://github.com/{repositoryOwner}/{repositoryName}/releases/download/{version}/Immortal.Vault.Setup.exe";
+    }
+    
+    public async Task<string?> GetLatestVersion(string repositoryOwner, string repositoryName)
+    {
+        var githubToken = this._configuration["GITHUB_TOKEN"];
+        if (githubToken is null)
+        {
+            throw new Exception("Github Token is missing");
+        }
+
+        var url = $"https://api.github.com/repos/{repositoryOwner}/{repositoryName}/releases/latest";
+
+        try
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Authorization = new AuthenticationHeaderValue("token", githubToken);
+            request.Headers.UserAgent.ParseAdd("ImmortalVault"); // GitHub требует наличие заголовка User-Agent
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.Error.WriteLine($"Network response was not ok: {response.ReasonPhrase}");
+                return null;
+            }
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var data = JObject.Parse(responseBody);
+            var tagName = data["tag_name"]?.Value<string>() ?? throw new Exception("Tag name is null");
+            
+            return tagName;
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Error fetching the latest release: {ex.Message}");
+            return null;
+        }
+    }
+}
